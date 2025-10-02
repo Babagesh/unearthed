2c204767365e7c67772d9391b37bb3580d705d12
Bhagyesh Jethwani <jethwani.bhagyesh@gmail.com>
2025-10-01 21:03:15 -0400

    individual gift routing added

--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -3,16 +3,11 @@ import path from 'path'
 import {fileURLToPath} from 'url'
 import GiftsController from '../controllers/gifts.js'
 
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
-
 const router = express.Router()
 
 router.get('/', GiftsController.getGifts)
 
-router.get('/:giftId', (req, res)=>{
-    res.status(200).sendFile(path.resolve(dirname, '../public/gift.html'))
-})
+router.get('/:giftId', GiftsController.getGiftById)
 
 export default router
 
